9e827bfced69bd444b6a5dd0eb73e50b0385b81f
Nguyen, Victor <vdn140030@gmail.com>
2020-12-29 22:11:23 -0600

    feat(index.*): emit message events

--- index.js
+++ index.js
@@ -7,9 +7,8 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', socket => {
-  console.log('A user connected!');
-  socket.on('disconnect', () => {
-    console.log('A user disconnected!');
+  socket.on('chat message', msg => {
+    console.log(`message: ${msg}`);
   });
 });
 
