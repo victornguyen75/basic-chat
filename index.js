4948c455c234ca2987d9a010ced02ba360bf84b6
Nguyen, Victor <vdn140030@gmail.com>
2020-12-29 22:05:26 -0600

    feat(index.*): integrate socket.io to watch for events

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  console.log('A user connected!');
+  socket.on('disconnect', () => {
+    console.log('A user disconnected!');
+  });
 });
 
 http.listen(3000, () => {
